cf9f48b75c4b78c39966dde602edd60c816e8b4f
w3WebTech <164163622+w3WebTech@users.noreply.github.com>
2024-12-24 11:19:10 +0530

    fix

---
+++ pages/funds.vue
@@ -0,0 +1,26 @@
+<template>
+      <div class="hidden md:block h-screen">
+        <!-- Iframe for larger screens -->
+        <iframe src="https://funds-plum.vercel.app/desktop.html" class="w-full h-screen border-2 border-gray-300"
+            title="Iframe Example 1"></iframe>
+    </div>
+    <div class="block md:hidden h-screen">
+        <!-- Iframe for smaller screens -->
+        <iframe src="https://funds-plum.vercel.app/testing.html" class="w-full h-screen border-2 border-gray-300"
+            title="Iframe Example 2"></iframe>
+    </div>
+</template>
+
+<script>
+import MobileFund from '../components/mobile-fund.vue'; // Adjust the path as necessary
+
+export default {
+  components: {
+    MobileFund
+  }
+}
+</script>
+
+<style scoped>
+/* Add any additional styles here */
+</style>
